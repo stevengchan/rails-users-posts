c386e220af0d2244d744d72955f825382b9952a5
stevengchan <stevengchan@gmail.com>
2013-05-30 01:14:13 -0700

    Fixed links in views to use urlhelper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,25 @@
 class UsersController < ApplicationController
+  # GET /users
   def index
   	@users = User.all
   end
 
+  # GET /users/1
   def show
     @user = User.find(params[:id])
   end
 
+  # GET /users/new
   def new
     @user = User.new
   end
 
+  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
+  # POST /users
   def create
     user_params = params.permit(:first_name, :last_name, :email_address, :password)
     @user = User.new(user_params)
@@ -25,11 +30,13 @@ class UsersController < ApplicationController
     end
   end
 
+  # PUT /users/1
   def update
     @user = User.find(params[:id])
-
+    user_params = params.permit(:first_name, :last_name, :email_address, :password)
   end
 
+  # DELETE /users/1
   def destroy
     @user = User.find(params[:id])
   end
